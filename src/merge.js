de1c7c53c0b846117647118eed9bee2ca091db44
Kevin Lu <6320810+kevinlul@users.noreply.github.com>
2023-04-15 21:29:42 -0400

    Add manual merge workflow for cached files

---
+++ src/merge.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+
+const aggregation = [];
+
+for (const filename of fs.readdirSync(".")) {
+    if (filename.endsWith(".json")) {
+        const file = fs.readFileSync(filename, { encoding: "utf-8" });
+        const card = JSON.parse(file);
+        aggregation.push(card);
+    }
+}
+
+fs.writeFileSync(process.argv[2], JSON.stringify(aggregation));
