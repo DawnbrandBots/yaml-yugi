756a2c4087f76ffd03d01e5bcfb609b9d67d295e
Kevin Lu <6320810+kevinlul@users.noreply.github.com>
2023-06-03 19:18:41 -0400

    Add workflow to automatically fetch TCG Forbidden & Limited Lists daily at 00:05 UTC
    #8

--- src/limit-regulation/tcg-all.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-for i in $(seq 54 $1); do
-    curl --silent "https://www.yugioh-card.com/uk/limited/?l=$i" | grep 'jsonData =' | yarn --ignore-engines ts-node src/limit-regulation/tcg-extract-transform.ts data/limit-regulation/tcg
-done
